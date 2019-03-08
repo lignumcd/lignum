c8fa2de73f92daf763bf6970936754051d96cda3
lignum <hello.talescript+lignumcd@gmail.com>
2019-03-07 23:03:54 -0500

    Finished portofolio section. Fix video on home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,33 +5,71 @@ window.onload = function () {
 
   const gallery = document.querySelectorAll(".gallery");
 
-  all.addEventListener("click", function(){
-    gallery.forEach(function(el){
-      el.classList.remove("d-none");
-      console.log(el);
+  /* Show all projects */
+  all.addEventListener("click", function () {
+    gallery.forEach(function (el) {
+      el.classList.remove("d-none", "scale-up-center");
+      el.classList.add("fade-in-tr");
+      /* ADD / Remove active class from buttons */
+      all.classList.add("active");
+      commercial.classList.remove("active");
+      residential.classList.remove("active");
     });
   });
 
-  commercial.addEventListener("click", function(){
-    gallery.forEach(function(el){
-      if (el.classList.contains("residential")){
+  /* Show commercial projects */
+  commercial.addEventListener("click", function () {
+    gallery.forEach(function (el) {
+      el.classList.remove("fade-in-tr");
+      if (el.classList.contains("residential")) {
         el.classList.add("d-none");
+        el.classList.remove("scale-up-center");
+        /* ADD / Remove active class from buttons */
+        commercial.classList.add("active");
+        residential.classList.remove("active");
+        all.classList.remove("active");
       }
-      if (el.classList.contains("commercial")){
+      if (el.classList.contains("commercial")) {
         el.classList.remove("d-none");
+        el.classList.add("scale-up-center");
       }
     });
   });
+  
+  /* Show residential projects */
+  residential.addEventListener("click", function () {
+    gallery.forEach(function (el) {
+      el.classList.remove("fade-in-tr");
+      if (el.classList.contains("commercial")) {
+        el.classList.add("d-none");
+        el.classList.remove("scale-up-center");
+        /* ADD / Remove active class from buttons */
+        residential.classList.add("active");
+        commercial.classList.remove("active");
+        all.classList.remove("active");
+      }
+      if (el.classList.contains("residential")) {
+        el.classList.remove("d-none");
+        el.classList.add("scale-up-center");
+      }
+    });
+  });/* End residential block */
 
-  residential.addEventListener("click", function(){
-    gallery.forEach(function(el){
-      if (el.classList.contains("commercial")){
-        el.classList.add("d-none");
-      }
-      if (el.classList.contains("residential")){
-        el.classList.remove("d-none");
-      }
-    });
-  });
+  /* ********************** */
+  /* INSERT IMAGES IN MODAL */
+  /* ********************** */
+  // parentElement.parentElement.children[0].getAttribute('src')
+  var insert_image = document.querySelector(".insert_image");
+    
+  document.body.addEventListener("click", function(evt){
+    evt = evt || window.event;
+    if (evt.target.classList[0] === "card-info"){
+      var target = evt.target || evt.srcElement
+    } 
+    //target.parentElement.parentElement.children[0].getAttribute('src');
+    console.log(target);
+    insert_image.src = target.parentElement.parentElement.children[0].getAttribute('src');
+
+  }, false);
 
 }
